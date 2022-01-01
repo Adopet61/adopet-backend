88e4ca7fbff04dda1db44f71c21fa093a7535c26
Fatma Nur <fatmanurtml@gmail.com>
2022-01-02 01:58:55 +0300

    Formdan alınacak veriler için kısıtlamalar ekle

--- project/src/main/java/adopet/project/entities/forms/AdoptForm.java
+++ project/src/main/java/adopet/project/entities/forms/AdoptForm.java
@@ -22,15 +22,25 @@ public class AdoptForm {
     private int formId;
 
     @Column(name = "fname")
+    @NotBlank
+    @NotNull
+    @Size(max = 20,message = "İsim uzunluğu en fazla 20 olabilir.")
     private String firstName;
 
     @Column(name = "lname")
+    @NotBlank
+    @NotNull
+    @Size(max = 20, message = "Soyisim uzunluğu en fazla 20 olabilir.")
     private String lastName;
 
     @Column(name = "phone_number")
+    @NotBlank
+    @NotNull
+    @Pattern(regexp ="[0-9//s]{10}",message = "Geçersiz numara!")
     private String phoneNumber;
 
     @Column(name = "email")
+    @Email
     private String email;
 
     @Column(name = "havepet")
--- project/src/main/java/adopet/project/entities/forms/MakeAdoptForm.java
+++ project/src/main/java/adopet/project/entities/forms/MakeAdoptForm.java
@@ -22,18 +22,29 @@ public class MakeAdoptForm {
     private int formId;
 
     @Column(name = "fname")
+    @NotBlank
+    @NotNull
+    @Size(max = 20,message = "İsim uzunluğu en fazla 20 olabilir.")
     private String firstName;
 
     @Column(name = "lname")
+    @NotBlank
+    @NotNull
+    @Size(max = 20,message = "Soyisim uzunluğu en fazla 20 olabilir.")
     private String lastName;
 
     @Column(name = "phone_number")
+    @NotBlank
+    @NotNull
+    @Pattern(regexp ="[0-9//s]{10}",message = "Geçersiz numara!")
     private String phoneNumber;
 
     @Column(name = "email")
+    @Email
     private String email;
 
     @Column(name = "pname")
+    @Size(max = 20,message = "İsim uzunluğu en fazla 20 olabilir.")
     private String animalName;
 
     @Column(name = "type")
